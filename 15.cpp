c53e8a82dbbc4fe73991abe60cc4a883690e165b
wulu90 <wulu90@outlook.com>
2021-12-16 16:00:09 +0800

    day 15 part2

--- 15.cpp
+++ 15.cpp
@@ -5,9 +5,11 @@
 #include <algorithm>
 #include <set>
 #include <limits>
+#include <queue>
 
 using namespace std;
 
+// https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm#Algorithm
 void djikstra(const vector<vector<int8_t>> &matrix)
 {
     // is square, row = col;
@@ -58,6 +60,61 @@ void djikstra(const vector<vector<int8_t>> &matrix)
     cout << dist[v_num - 1] - matrix[0][0] + matrix[row - 1][row - 1] << endl;
 }
 
+void djikstra_priority_queue(const vector<vector<int8_t>> &matrix)
+{
+    // is square, row = col;
+    int row = matrix.size();
+    // use Djikstra, vertex num is row*row
+    int v_num = row * row;
+
+    int32_t inf = numeric_limits<int32_t>::max();
+
+    vector<int32_t> dist(v_num, inf); // current distance from the 0 to the vertex i
+    vector<bool> visited(v_num, false);
+    priority_queue<pair<int32_t, int32_t>, vector<pair<int32_t, int32_t>>, greater<pair<int32_t, int32_t>>> pqueue; // distance,vertex_index;
+    dist[0] = 0;
+    //visited[0] = true;
+    int cur = 0;
+
+    //vector<vector<int8_t>> dirs{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    while (true)
+    {
+        vector<int32_t> neighbors{cur - row, cur + row, cur - 1, cur + 1};
+        for (auto &nei : neighbors)
+        {
+            if (nei < 0 || nei > v_num - 1 || (cur / row != nei / row && cur % row != nei % row))
+                continue;
+            if (!visited[nei] && dist[nei] > dist[cur] + matrix[cur / row][cur % row])
+            {
+                dist[nei] = dist[cur] + matrix[cur / row][cur % row];
+                pqueue.push({dist[nei], nei});
+            }
+        }
+        visited[cur] = true;
+
+        if (visited[v_num - 1])
+        {
+            break;
+        }
+        else
+        {
+            cur = pqueue.top().second;
+            pqueue.pop();
+            // int min = inf;
+            // for (int i = 0; i < v_num; i++)
+            // {
+            //     if (!visited[i] && dist[i] < min)
+            //     {
+            //         min = dist[i];
+            //         cur = i;
+            //     }
+            // }
+        }
+    }
+
+    cout << dist[v_num - 1] - matrix[0][0] + matrix[row - 1][row - 1] << endl;
+}
+
 void part1()
 {
     ifstream ifs{"input\\input-15"};
@@ -76,7 +133,8 @@ void part1()
         matrix.push_back(line_vec);
     }
 
-    djikstra(matrix);
+    //djikstra(matrix);
+    djikstra_priority_queue(matrix);
 }
 
 void part2()
@@ -119,11 +177,11 @@ void part2()
         }
     }
 
-    djikstra(full_matrix);
+    djikstra_priority_queue(full_matrix);
 }
 
 int main()
 {
-    //part1();
+    part1();
     part2();
 }
