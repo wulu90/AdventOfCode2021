71f713fd216f56ca2258a809b3464b0199810885
wulu <wulu90@outlook.com>
2023-09-30 23:47:44 +0800

    day 24 part2

--- 24-1.cpp
+++ 24-1.cpp
@@ -6,14 +6,14 @@
 
 using namespace std;
 
-vector<int> divs{1, 1, 1, 1, 26, 1, 26, 1, 1, 26, 26, 26, 26, 26};
-vector<int> addx{15, 15, 12, 13, -12, 10, -9, 14, 13, -14, -11, -2, -16, -14};
-vector<int> addy{15, 10, 2, 16, 12, 11, 5, 16, 6, 15, 3, 12, 10, 13};
+static vector<int> divs{1, 1, 1, 1, 26, 1, 26, 1, 1, 26, 26, 26, 26, 26};
+static vector<int> addx{15, 15, 12, 13, -12, 10, -9, 14, 13, -14, -11, -2, -16, -14};
+static vector<int> addy{15, 10, 2, 16, 12, 11, 5, 16, 6, 15, 3, 12, 10, 13};
 
-vector<int> digits{9, 8, 7, 6, 5, 4, 3, 2, 1};
-set<pair<int, int>> z_depth;
+// vector<int> digits{9, 8, 7, 6, 5, 4, 3, 2, 1};
+static set<pair<int, int>> z_depth;
 
-int64_t generate(int z, int64_t num, int depth) {
+int64_t generate(int z, int64_t num, int depth, const vector<int>& digits) {
     if (z_depth.contains({z, depth}) || depth == 14) {
         return numeric_limits<int64_t>::max();
     }
@@ -47,7 +47,7 @@ int64_t generate(int z, int64_t num, int depth) {
             return num + digits[i];
         }
 
-        int64_t ans = generate(z, num + digits[i], depth + 1);
+        int64_t ans = generate(z, num + digits[i], depth + 1, digits);
 
         if (ans != numeric_limits<int64_t>::max()) {
             return ans;
@@ -59,5 +59,7 @@ int64_t generate(int z, int64_t num, int depth) {
 }
 
 int main() {
-    cout << generate(0, 0, 0) << endl;
+    cout << generate(0, 0, 0, {9, 8, 7, 6, 5, 4, 3, 2, 1}) << endl;
+    z_depth.clear();
+    cout << generate(0, 0, 0, {1, 2, 3, 4, 5, 6, 7, 8, 9}) << endl;
 }
